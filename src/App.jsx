6343a825ecc7304442347a0de31ab023469b4d53
니터로닝 <knitterronin@naver.com>
2026-04-02 17:08:26 +0900

    Ver1.0 - 기본 기능코드 작성 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { useState } from 'react';
 
-const App = () => {
+function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div>App</div>
-  )
+    <div>
+      <h1>Counter : {count}</h1>
+      {/* 1. 정상: 익명 함수로 감싸서 전달 */}
+      <button onClick={() => setCount(count - 1)}>빼기</button>
+      
+      {/* 2. 수정: 반드시 () => 를 붙여서 클릭했을 때만 실행되게 함 */}
+      <button onClick={() => setCount(0)}>reset</button>
+      
+      {/* 3. 수정: 매개변수 자리를 비우거나 e(event)를 넣고, 로직을 +로 변경 */}
+      <button onClick={() => setCount(count + 10)}>더하기</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
